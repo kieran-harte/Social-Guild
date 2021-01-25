a514160cf2dd9ab64fbe9f6711a13b65f6a03332
Kieran Harte <work@kieran.io>
2021-01-25 09:16:15 +0000

    install security packages

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,11 @@ const dotenv = require('dotenv')
 const cookieParser = require('cookie-parser')
 const cors = require('cors')
 require('colors')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const rateLimit = require('express-rate-limit')
+const hpp = require('hpp')
 const errorHandler = require('./middleware/error')
-
 // Load .env
 const dotenvConf = dotenv.config({ path: './backend/config/.env' })
 if (dotenvConf.error) throw dotenvConf.error
@@ -22,6 +25,16 @@ app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser(process.env.COOKIE_SECRET))
 app.use(morgan(process.env.NODE_ENV === 'dev' ? 'dev' : 'common'))
+app.use(helmet())
+app.use(xss())
+app.use(hpp())
+
+// Limit requests to 1000 per minutes
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 1000
+})
+app.use(limiter)
 
 // Mount API routes
 app.use('/api/v1/auth', require('./routes/auth'))
